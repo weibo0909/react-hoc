d1d0b91edcaf54fb0d877284395ca95fd3c07b05
sunday <sunday.wei@derbysoft.com>
2019-06-19 09:15:18 +0800

    test

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,23 +1,23 @@
-import { createStore } from 'redux';
+import {createStore} from 'redux';
 import reducerFunc from './reducers';
 
 const store = createStore(reducerFunc);
 
 console.log(store.getState());
 
-const unsubscribe = store.subscribe(()=>{
+const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
-})
+});
 
 
 store.dispatch({
-    type:'set_user',
-    userInfo:{name:'wjk'}
+    type: 'set_user',
+    userInfo: {name: 'wjk'}
 });
 
 store.dispatch({
-    type:'set_base_info',
-    baseInfo:{base:'hello'}
+    type: 'set_base_info',
+    baseInfo: {base: 'hello'}
 });
 
 
--- src/study/main.js
+++ src/study/main.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
-import TestComponent from "./TestComponent";
+import {render} from 'react-dom';
+import TestComponent from './TestComponent';
 // import 'antd/dist/antd.css';
 
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from '../redux/createStore';
 
-render(<Provider store={store}><TestComponent /></Provider>,document.getElementById('body'));
+render(<Provider store={store}><TestComponent/></Provider>, document.getElementById('body'));
 
 
